8fbe7a08e0b604f453b43d4141eeca629ee77086
Pete Lowth <peter@lowth.com>
2020-11-22 19:36:37 +0000

    Predict from homepage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, render_template, url_for
 import numpy as np
 import pandas as pd
 from model import prepare_data, match_companies_knn, build_model
@@ -12,6 +12,26 @@ def makecalc():
     df_prediction = match_companies_knn(test_companies, vectorizer, model, df, ignore_words)
 
     return df_prediction.to_json(orient="records")
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    test_companies = [x for x in request.form.values()]
+
+    df_prediction = match_companies_knn(test_companies, vectorizer, model, df, ignore_words)
+
+    n_neighbors = model.get_params()['n_neighbors']
+
+    df_prediction_reshape = pd.DataFrame({
+        'CompanyNumber': [df_prediction[f'match.{i}.CompanyNumber'][0] for i in range(n_neighbors)],
+        'CompanyName': [df_prediction[f'match.{i}.CompanyName'][0] for i in range(n_neighbors)],
+        'Score': [df_prediction[f'match.{i}.Distance'][0] for i in range(n_neighbors)]
+    })
+
+    return render_template("home.html", pred=df_prediction_reshape.to_html())
 
 
 if __name__ == '__main__':
